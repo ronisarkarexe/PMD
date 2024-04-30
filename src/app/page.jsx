a244960a0031cf8bdcaa0caba769d7ce6bff6881
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-04-30 13:50:02 +0530

    changes file

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1>Hello World</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
